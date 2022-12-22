c9bb9f683cb12a2ea06cf338f86a0bebc7916015
quan19tomosia <tt.quan.nguyen@tomosia.com>
2022-12-22 11:11:36 +0700

    Add last visit for patients and calculate rate for physicians

--- app/helpers/patients_helper.rb
+++ app/helpers/patients_helper.rb
@@ -1,2 +1,6 @@
 module PatientsHelper
+  def last_visit(patient)
+    last_visit = Appointment.where(patient_id: patient.id).where(state: 2).order(schedule: :desc).pluck(:schedule).first
+    return last_visit ? last_visit.strftime("%d-%m-%Y") : "N/A"
+  end
 end
--- app/helpers/physicians_helper.rb
+++ app/helpers/physicians_helper.rb
@@ -1,2 +1,12 @@
 module PhysiciansHelper
+  def rate(physician)
+    appoointments = Appointment.joins(:physician).where(physician_id: physician.id).where(state: 2).where.not(rate: nil).pluck(:rate)
+    count = appoointments.count
+    sum = appoointments.inject(0) { |sum, x| sum + x }
+    return (sum / count).to_f unless count == 0
+  end
+
+  def complete_count(physician)
+    return Appointment.where(physician_id: physician.id).where(state: 2).count
+  end
 end
